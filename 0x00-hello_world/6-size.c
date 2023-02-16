bfc87324b45c686e924f634b17c20eb0fc7b39ac
mahmoudramy <mahmoudramy84@gmail.com>
2023-02-16 13:58:07 -0800

    Solve Tasks

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
-int mai(void)
+/**
+ * main - Prints sized of characters
+ *
+ *Return: Always 0 (success)
+ */
+int main(void)
 {
 printf("Size of a char: %zu byte(s)\n", sizeof(char));
 printf("Size of an int: %zu byte(s)\n", sizeof(int));
