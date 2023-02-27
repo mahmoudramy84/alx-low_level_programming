5a68c1403dfbeae011e7b4084b67bd07ba3c62f6
mahmoudramy <mahmoudramy84@gmail.com>
2023-02-27 05:29:50 -0800

    Update task 0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
- * reset_to_98 - function take int as parameter and updates the value it points to to 98.
+ * reset_to_98 -function take int as parameter updates the value it points to 98
  *
  * @n : an intger
  */
