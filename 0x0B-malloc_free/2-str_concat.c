d7767b4ffa803ff51584593fdbd66b7f4e3a05d6
mahmoud elbehery <mahmoudramy84@gmail.com>
2023-03-15 15:05:58 +0200

    Added task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,39 +1,40 @@
 #include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
 
 /**
- * _str_concat - concatenates two strings.
+ * str_concat - concatenate two strings using malloc
  * @s1: string 1
  * @s2: string 2
  * Return: pointer to concat string
  */
+
 char *str_concat(char *s1, char *s2)
 {
-    char *sum;
-    int a, b, c, d;
+	char *a;
+	int i, j, c, d;
 
-    if (s1 == NULL)
-        s1 = "";
-    if (s2 == NULL)
-        s2 = "";
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 
-    for (a = 0; s1[a] != '\0'; a++)
-        ;
-    for (b = 0; s2[b] != '\0'; b++)
-        ;
+	for (i = 0; s1[i] != '\0'; i++)
+		;
+	for (j = 0; s2[j] != '\0'; j++)
+		;
 
-    sum  = malloc((a * sizeof(*s1)) + (b * sizeof(*s2)) + 1);
-	if (sum == NULL)
+	a = malloc((i * sizeof(*s1)) + (j * sizeof(*s2)) + 1);
+	if (a == NULL)
 		return (NULL);
 
-	for (c = 0, d = 0; c < (a + b + 1); c++)
+	for (c = 0, d = 0; c < (i + j + 1); c++)
 	{
-		if (c < d)
-			sum[c] = s1[c];
+		if (c < i)
+			a[c] = s1[c];
 		else
-			sum[c] = s2[d++];
+			a[c] = s2[d++];
 	}
 
-	return (sum);
+	return (a);
 }
+
