8f83b4191063830d99339ff03a458edb3a0f5283
mahmoud elbehery <mahmoudramy84@gmail.com>
2023-03-23 23:06:25 +0200

    Added tasks solutions

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,27 +1,28 @@
 #include "variadic_functions.h"
 #include <stdarg.h>
+#include <stdio.h>
 /**
- * sum_them_all - function that returns the sum of all its parameters.
+ * print_numbers - function that prints numbers.
  * @n: resepresents the number of arguments
+ * @separator: resepresents the seperator b/w the numbers
  * Return: Always 0.
  */
-int sum_them_all(const unsigned int n, ...)
+void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	/* creating va_list to store the variable argument list */
 	va_list my_nums;
 	unsigned int count;
-	int sum;
-
-	if (n == 0)
-		return (0);
-	sum = 0;
 
 	/* init valist for the number of arguments */
 	va_start(my_nums, n);
 
 	/* loop through/access all arguments stored in the valist */
 	for (count = 0; count < n; count++)
-		sum += va_arg(my_nums, int);
+	{
+		printf("%d", va_arg(my_nums, unsigned int));
+		if (count < (n - 1) && separator != NULL)
+			printf("%s", separator);
+	}
 	va_end(my_nums);
-	return (sum);
+	printf("\n");
 }
