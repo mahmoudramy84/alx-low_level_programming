47c55d8d22c12914bc5cf3b832dfdd0a801b7690
mahmoud elbehery <mahmoudramy84@gmail.com>
2023-04-04 18:04:17 +0200

    Upated tasks Code style

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -1,11 +1,11 @@
 #include "lists.h"
 
 /**
- *free_listint -frees a listint_t list.
- *@head: Pointer to list.
- *Return: void.
+ * free_listint - frees a listint_t list
+ * @head: Pointer to list
+ *
+ * Return: void
  */
-
 void free_listint(listint_t *head)
 {
     listint_t *temp;
