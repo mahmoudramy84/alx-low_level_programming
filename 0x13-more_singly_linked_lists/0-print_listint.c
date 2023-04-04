48f766c3bf8d3547b323a66a6385eca9ae5a117b
mahmoud elbehery <mahmoudramy84@gmail.com>
2023-04-04 19:24:09 +0200

    Upated tasks Code style

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,21 +1,19 @@
 #include "lists.h"
-
 /**
  * print_listint -prints all the elemnts of alist.
  * @h: singly linked list.
- *
  * Return: the numbers of node.
  */
 size_t print_listint(const listint_t *h)
 {
-    size_t nelem;
-    nelem = 0;
+size_t nelem;
+nelem = 0;
 
-    while (h != NULL)
-    {
-        printf("%d\n", h->n);
-        h = h->next;
-        nelem++;
-    }
-    return (nelem);
+while (h != NULL)
+{
+printf("%d\n", h->n);
+h = h->next;
+nelem++;
+}
+return (nelem);
 }
