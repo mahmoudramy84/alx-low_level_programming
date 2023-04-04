b2b743d5a93bc50a7e97471ccb13ca83e7c05b9f
mahmoud elbehery <mahmoudramy84@gmail.com>
2023-04-04 19:11:49 +0200

    Upated tasks Code style

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -7,24 +7,24 @@
  */
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
-    listint_t *new;
-	listint_t *cursor = *head;
+listint_t *new;
+listint_t *cursor = *head;
 
-	new = malloc(sizeof(listint_t));
-	if (new != NULL)
-	{
-		new->n = n;
-		new->next = NULL;
-	}
-	else
-		return (NULL);
-	if (cursor != NULL)
-	{
-		while (cursor->next != NULL)
-			cursor = cursor->next;
-		cursor->next = new;
-	}
-	else
-		*head = new;
-	return (new);
+new = malloc(sizeof(listint_t));
+if (new != NULL)
+{
+new->n = n;
+new->next = NULL;
+}
+else
+return (NULL);
+if (cursor != NULL)
+{
+while (cursor->next != NULL)
+cursor = cursor->next;
+cursor->next = new;
+}
+else
+*head = new;
+return (new);
 }
