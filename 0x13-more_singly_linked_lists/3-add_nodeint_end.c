250f2b1028e5a8e253c768d7ffffceff825ab8b8
mahmoud elbehery <mahmoudramy84@gmail.com>
2023-04-04 19:10:00 +0200

    Upated tasks Code style

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -1,10 +1,8 @@
 #include "lists.h"
-
 /**
  * add_nodeint_end -adds a new node at the end of list
- * @head: Pointer to pointer
+ * @head: Pointer to list
  * @n: Number to add it
- *
  * Return: address of the element, or NULL if failed
  */
 listint_t *add_nodeint_end(listint_t **head, const int n)
