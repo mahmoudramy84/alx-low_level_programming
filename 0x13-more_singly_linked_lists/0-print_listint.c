0f1114e074bf4f341ac3d1287df9936f49e3cd39
mahmoud elbehery <mahmoudramy84@gmail.com>
2023-04-04 11:32:00 +0200

    Upatedtasks Code style

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,15 +1,16 @@
 #include "lists.h"
+
 /**
  *print_listint -prints all the elemnts of a listint_t list.
  *@h: singly linked list.
+ *
  * Return: the numbers of node.
  */
-
 size_t print_listint(const listint_t *h)
 {
     size_t nelem;
-
     nelem = 0;
+
     while (h != NULL)
     {
         printf("%d\n", h->n);
--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,15 +1,16 @@
 #include "lists.h"
+
 /**
  *listint_len -Returns the number of elements in a linked listint_t list.
  *@h: Pointer to single linked list.
+ *
  *Return: the number of elements.
  */
-
 size_t listint_len(const listint_t *h)
 {
     size_t nelem;
-
     nelem = 0;
+
     while (h != NULL)
     {
         h = h->next;
