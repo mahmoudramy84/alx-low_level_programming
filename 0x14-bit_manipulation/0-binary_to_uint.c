9a4e0a19d5006fe72d2282a87656bd9d37ce2192
mahmoud elbehery <mahmoudramy84@gmail.com>
2023-04-09 17:35:52 +0200

    Updated 0-binary_to_uint task

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -13,16 +13,18 @@ unsigned int binary_to_uint(const char *b)
     }
 
     unsigned int result = 0;
-    int i = 0;
+    int i;
 
-    while (b[i] != '\0')
-    {
-        if (b[i] != '0' && b[i] != '1')
+    for (i = 0; b[i] != '\0'; i++)
+	{
+		if (b[i] != '0' && b[i] != '1')
 			return (0);
-        result <<= 1;
+	}
+	for (i = 0; b[i] != '\0'; i++)
+	{
+		result <<= 1;
 		if (b[i] == '1')
 			result += 1;
-        i++;
-    }
+	}
     return result;
 }
