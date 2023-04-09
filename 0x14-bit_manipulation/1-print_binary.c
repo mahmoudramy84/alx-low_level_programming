bad2462ab7199d0a4dba6a5fb804a1a4ed44556a
mahmoud elbehery <mahmoudramy84@gmail.com>
2023-04-09 18:10:32 +0200

    Updated 1-print_binary task file

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -9,17 +9,23 @@ void print_binary(unsigned long int n)
 {
     int num_bits = sizeof(unsigned long int) * 8;
     unsigned long int mask = 1UL << (num_bits - 1);
-
+    int leading_zeros = 1;
     while (mask)
     {
         if (n & mask)
         {
             _putchar('1');
+            leading_zeros = 0;
         }
-        else
+        else if (!leading_zeros)
         {
             _putchar('0');
         }
         mask >>= 1;
+    }
+
+    if (leading_zeros)
+    {
+        _putchar('0');
     }
 }
