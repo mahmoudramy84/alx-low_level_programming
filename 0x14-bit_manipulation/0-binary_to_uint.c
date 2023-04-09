f6ae934e1d6d83ce9cccdaffe71a8e9002e0a994
mahmoud elbehery <mahmoudramy84@gmail.com>
2023-04-09 14:51:02 +0200

    Create new tasks directory, added header file, README file, 0-binary_to_uint0 task file

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * binary_to_uint - converts a binary number to an unsigned int
+ * @b: is pointing to a string of 0 and 1 chars
+ * Return: the converted number
+ */
+unsigned int binary_to_uint(const char *b)
+{
+    if (b == NULL)
+    {
+        return 0;
+    }
+
+    unsigned int result = 0;
+    int i = 0;
+
+    while (b[i] != '/0')
+    {
+        if (b[i] == '0')
+            result <<= 1;
+        else if (b[i] == '1')
+            result << '1' | 1;
+        else
+            return 0;
+        i++;
+    }
+    return result;
+}
