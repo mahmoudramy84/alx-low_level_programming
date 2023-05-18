8353d6a1ae2c3b7330346013527a7ddcc2e5c065
mahmoudramy <mahmoudramy84@gmail.com>
2023-05-18 05:56:26 -0700

    Updating 0-print_dlistint.c task solution

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * print_dlistint - prints all elements 
+ * print_dlistint - prints all elements
  * @h: head of the list
  * Return: the number of nodes
  */
@@ -26,4 +26,4 @@ size_t print_dlistint(const dlistint_t *h)
 
 	return (count);
 }
-	
+
