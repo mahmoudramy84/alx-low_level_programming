a1ce548035c39f2847e9a9546c92810733b8893b
mahmoudramy <mahmoudramy84@gmail.com>
2023-05-18 19:46:50 -0700

    adding 4-free_dlistint.c task solution

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -11,9 +11,9 @@ void free_dlistint(dlistint_t *head)
 	dlistint_t *tmp;
 
 	if (head != NULL)
-		while (head->prev != Null)
+		while (head->prev != NULL)
 			head = head->prev;
-	while ((tmp = head) != Null)
+	while ((tmp = head) != NULL)
 	{
 		head = head->next;
 		free(tmp)
